0a1e1ada2f79752c231fd1f9f50dc393d249c5dc
francois-michaut <f.michaut@orange.fr>
2022-05-06 12:34:19 +0200

    Legal page done

--- src/components/Legal/index.js
+++ src/components/Legal/index.js
@@ -7,6 +7,67 @@ function Legal() {
   return (
     <main className="legal">
       <h1>Mentions Légales</h1>
+      <p>En vigueur au 05/05/2022</p>
+      <section>
+        <p>
+          Conformément aux dispositions des Articles 6-III
+          et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
+          Confiance dans l'économie numérique, dite L.C.E.N.,
+          il est porté à la connaissance des utilisateurs et
+          visiteurs, ci-après l'"Utilisateur", du site "O'Dungeons",
+          ci-après le "Site", les présentes mentions légales.
+        </p>
+        <p>
+          La connexion et la navigation sur le Site par l'Utilisateur
+          implique acceptation intégrale et sans réserve des présentes mentions légales.
+        </p>
+        <p>
+          Ces dernières sont accessibles sur le Site à la rubrique
+          "Mentions légales".
+        </p>
+        <h2>Article1-l'editeur</h2>
+        <p>
+          ci-après l'"Editeur".
+        </p>
+        <h2>article2-l'hebergeur</h2>
+        <p>
+          L'hébergeur du Site est la société 'Quinenveut', dont le siège
+          social est situé au 222 quai de la fosse 44000 Nantes, avec le
+          numéro de téléphone: +336569 et l'adresse mail:
+          jenpeutplus@skip.fr .
+        </p>
+        <h2>article3-accès au site</h2>
+        <p>
+          Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas
+          de force majeure, interruption programmée ou non et pouvant
+          découler d'une nécessité de maintenance.
+        </p>
+        <p>
+          En cas de modification, interruption ou suspension du Site,
+          l'Editeur ne saurait être tenu responsable.
+        </p>
+        <h2>article4-collecte des données</h2>
+        <p>
+          Le Site assure à l'Utilisateur une collecte et un
+          traitement d'informations personnelles dans le respect de
+          la vie privée conformément à la loi n°78-17 du 6 janvier 1978
+          relative à l'informatique, aux fichiers et aux libertés.
+        </p>
+        <p>
+          En vertu de la loi Informatique et Libertés, en date du 6
+          janvier 1978, l'Utilisateur dispose d'un droit  d'accès,
+          de rectification, de suppression et d'opposition de ses
+          données personnelles. L'Utilisateur exerce ce droit:
+        </p>
+        <p>
+          Toute utilisation, reproduction, diffusion, commercialisation,
+          modification de toute ou partie du Site, sans autorisation de
+          l'Editeur est prohibée et pourra entraînée des actions et
+          poursuites judiciaires telles que notamment prévues par le
+          Code de la propriété intellectuelle et le Code Civil.
+        </p>
+        <p>Rédigé sur http://legalplace.fr</p>
+      </section>
     </main>
   );
 }

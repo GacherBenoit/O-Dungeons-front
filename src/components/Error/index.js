64e8102920ec744315e1be67c62b897016c01acf
francois-michaut <f.michaut@orange.fr>
2022-05-06 15:46:21 +0200

    Error page created

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,12 +1,17 @@
 // == import: local
-import './error.scss';
+import { NavLink } from 'react-router-dom';
 
+import './error.scss';
+import orcsarmy from '../../assets/images/orcarmy.png';
 // == Composant
 
 function Error() {
   return (
     <main className="error">
-      <h1>Classes</h1>
+      <h1>Oupsss ... tu es tombé dans une embuscade !!</h1>
+
+      <button type="button"><NavLink to="/">Retour à l'accueil</NavLink></button>
+      <img src={orcsarmy} alt="orcs" />
     </main>
   );
 }

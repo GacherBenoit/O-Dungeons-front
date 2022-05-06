3ef5e0eaa67785fa0e7b0f84c316d0718494e683
AlbanBerthn <a.bertholon69@gmail.com>
2022-05-06 09:46:11 +0200

    installation of react-router-dom and setting route for presentation components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,7 @@
+// == Import: npm
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+
 // == Import
 import './home.scss';
 
@@ -7,6 +11,10 @@ import Presentation from '../Presentation';
 import Footer from '../Footer';
 
 function Home() {
+  // Le hook useLocation permet de récupérer des informations
+  // sur l'emplacement courant (url) et abonne le composant
+  // qui l'utilise a la modification de l'url
+  const location = useLocation();
   return (
     <div className="home">
       <Header />

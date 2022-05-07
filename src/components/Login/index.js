85bbecd38d0ff8b732e400e371ee526e99493437
AlbanBerthn <a.bertholon69@gmail.com>
2022-05-07 13:28:39 +0200

    desktop version ok

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,9 +3,41 @@ import './login.scss';
 
 // == Composant
 function Login() {
+  // Afin d'eviter le rechargement de la page lors de la soumission du formulaire
+  // On creer une fonction qui annule le comportoment par defaut de ce dernier
+  function handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
   return (
     <main className="login">
-      <h1>Connexion</h1>
+      <h1 className="login__title">Connexion</h1>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          <span className="login__form--title">Email</span>
+          <input
+            type="email"
+            className="login__form--input"
+            placeholder="Saisissez votre email..."
+            value=""
+          />
+        </label>
+
+        <label htmlFor="password">
+          <span className="login__form--title">Mot de passe</span>
+          <input
+            type="password"
+            className="login__form--input"
+            placeholder="Saisissez votre mot de passe"
+            value=""
+          />
+        </label>
+
+        <button type="submit" className="login__form--button">
+          Se Connecter
+        </button>
+
+      </form>
     </main>
   );
 }

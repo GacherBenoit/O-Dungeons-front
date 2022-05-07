b3a21fd4af4a6203a9a9ef624b6d750bb280ec65
AlbanBerthn <a.bertholon69@gmail.com>
2022-05-07 14:24:00 +0200

    action type and action creator for menu burger in mobile version created

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+// ACTION TYPE MENU_BURGER_TOGGLE
+export const MENU_BURGER_TOGGLE = 'MENU_BURGER_TOGGLE';
+
+// ACTION CREATOR menuBurgerToggle
+export const menuBurgerToggle = () => ({
+  type: MENU_BURGER_TOGGLE,
+});

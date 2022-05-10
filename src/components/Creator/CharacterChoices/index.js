c9ab458a6f3e83f63ddc41841e18e1953e4dec56
francois-michaut <f.michaut@orange.fr>
2022-05-10 10:27:56 +0200

    Component Characterchoices finished mobile and desktop

--- src/components/Creator/CharacterChoices/index.js
+++ src/components/Creator/CharacterChoices/index.js
@@ -1,13 +1,13 @@
 // == Import: local
 import './characterChoices.scss';
-import barbarian from '../../../assets/images/barbarian.jpg';
+import warrior from '../../../assets/images/guerriertransparent.png';
 
 // == Composant
 
 function CharacterChoices() {
   return (
     <div className="choices">
-      <h2>character Choices</h2>
+      <h3>Character Choices</h3>
       <input type="text" placeholder="Nom du personnage" />
       <label htmlFor="choices-race" />
       <select className="choices__race" id="choices-race">
@@ -38,7 +38,7 @@ function CharacterChoices() {
         <option value="elementalist">Mage élémentaire</option>
       </select>
       <div className="choices__results">
-        <ul>
+        <ul className="choices__results--list">
           <li>Classe: Guerrier</li>
           <li>Race: Humain</li>
           <li>Background: Hors-la-loi</li>
@@ -46,7 +46,7 @@ function CharacterChoices() {
           <li>Nom du joueur: Alban</li>
           <li>Points d'experience: 0</li>
         </ul>
-        <img src={barbarian} alt="barabre" className="choices__results--img" />
+        <img src={warrior} alt="barabre" className="choices__results--img" />
       </div>
     </div>
   );

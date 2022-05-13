9993f30d5a5048112b49755e1e91bba8710401e1
francois-michaut <f.michaut@orange.fr>
2022-05-13 10:56:55 +0200

    Component Subclasses and ClasseItem finished and dynamics

---
+++ src/components/Subclasses/ClasseItem/index.js
@@ -0,0 +1,52 @@
+import { useSelector } from 'react-redux';
+
+import './classeItem.scss';
+
+function ClasseItem() {
+  const currentClasse = useSelector((state) => state.classes.currentClasse);
+  console.log(currentClasse);
+  return (
+    <div className="items">
+      <h2 className="items__title">Spécificités de la classe</h2>
+      <p className="items__dices">Dé de vie: {currentClasse.lifeDice}</p>
+      <div className="items__armor">
+        <h3>Armure</h3>
+        {(currentClasse.armors).map((armor) => (
+          <div key={armor.id}>
+            <p>Nom de l'armure: {armor.name}</p>
+            <p>Type d'armure : {armor.armorType}</p>
+            <p>Classe d'armure : {armor.armorClass}</p>
+            <p>Discrétion : {armor.discretion ? armor.discretion : 'aucune'}</p>
+            <p>Poids : {armor.weight}kg </p>
+            <p>Force : {armor.strength}</p>
+          </div>
+        ))}
+        <h3>Arme</h3>
+        <div className="items__weapons">
+          {(currentClasse.weapons).map((weapon) => (
+            <div key={weapon.id}>
+              <p>Nom de l'arme : {weapon.name}</p>
+              <p>Type de l'arme : {weapon.type} </p>
+              <p>Propriété de l'arme : {weapon.property} </p>
+              <p>Dé de dommages : {weapon.damageDice} </p>
+              <p>Type de dommages : {weapon.damageType} </p>
+              <p>Poids de l'arme : {weapon.weight} </p>
+            </div>
+          ))}
+        </div>
+        <h3>Objets</h3>
+        <div className="items__class-items">
+          {(currentClasse.playableClassItems).map((item) => (
+            <div key={item.item.id}>
+              <p>Nom de l'objet :{item.item.name}</p>
+              <p>Quantité possédée : {item.quantity} </p>
+              <p>Poids de l'objet :{item.item.weight}Kg</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClasseItem;
--- src/components/Subclasses/index.js
+++ src/components/Subclasses/index.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 
 // == Import : local
 import Loading from '../Loading';
+import ClasseItem from './ClasseItem';
 import './subclasses.scss';
 
 function Subclasses() {
@@ -12,16 +13,6 @@ function Subclasses() {
   const currentClasse = useSelector((state) => state.classes.currentClasse);
   const currentSubClasse = useSelector((state) => state.classes.currentClasse.subclasses);
 
-  // const informationClasse = { ...currentClasse };
-  // const { currentClasse: { subclasses } } = useSelector((state) => state.classes.currentClasse);
-  // const currentSubClasses = useSelector((state) => state.classes.currentClasse.subclasses);
-  // const { id, name, description } = currentSubClasses;
-  // // console.log(subclasses);
-  // // console.log(informationClasse);
-  // console.log(currentClasse);
-  // console.log(currentSubClasse);
-  // console.log(currentclasse.subclasses.name);
-  // console.log(subClasses);
   if (loading) {
     return <Loading />;
   }
@@ -30,21 +21,19 @@ function Subclasses() {
       <section className="subclasses__mainclass" id="subclasses">
         <h2 className="subclasses__mainclass--title">{currentClasse.name}</h2>
         <img className="subclasses__img" src={currentClasse.imageUrl} alt="classe" />
-        <p>{currentClasse.description}</p>
+        <p className="subclasses__paragraphe">{currentClasse.description}</p>
+        <ClasseItem />
       </section>
       <h2>Sous-classe</h2>
       <section className="subclasses__presentation">
-        {console.log(currentClasse.subclasses)}
         {/* On verifie si c'est defini et si c'est defini, on peu boucler dessus
         ressource :
         https://stackoverflow.com/questions/67601748/typeerror-map-is-not-a-function-using-react-js */}
-        { currentClasse.subclasses && currentClasse.subclasses.map((subclasse) => (
-          <div className="subclasses__presentation--card" id={subclasse.id}>
+        { currentSubClasse && currentClasse.subclasses.map((subclasse) => (
+          <div className="subclasses__presentation--card" id={subclasse.id} key={subclasse.id}>
             <h3>{subclasse.name}</h3>
             <p>
               <span>{subclasse.description}</span>
-              : elfe parcourant les océans à dos d'hippocampe.Porteur
-              d'une armure en écailles, ils sont de vrais nageurs....
             </p>
             <p>
               <span>Trait</span>: force moyenne, courage sans égal, espiègle.

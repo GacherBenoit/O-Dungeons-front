cda43e0f744feb5c2fa1b13b1738e4cbacae183e
francois-michaut <f.michaut@orange.fr>
2022-05-17 16:47:25 +0200

    Component trait is dynamic and ok

--- src/components/Creator/Traits/index.js
+++ src/components/Creator/Traits/index.js
@@ -1,19 +1,14 @@
+import { useSelector } from 'react-redux';
 import './traits.scss';
 
 function Traits() {
+  const trait = useSelector((state) => state.character.currentSubrace.trait);
   return (
     <div className="traits">
       <h4 className="traits--title">Fonction et traits</h4>
       <p className="traits__paragraphe">
-        Les traits correspondants à votre classe sont:
+        {trait}
       </p>
-      <ul className="traits__paragraphe--list">
-        <li>augmentation de caractéristiques</li>
-        <li>âge</li>
-        <li>taille</li>
-        <li>vitesse</li>
-        <li>bravoure</li>
-      </ul>
     </div>
   );
 }

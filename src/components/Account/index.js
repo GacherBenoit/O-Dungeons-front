6a64d7ae65e1dff6330c68d3f39b3b8640b27b36
AlbanBerthn <a.bertholon69@gmail.com>
2022-05-18 12:34:16 +0200

    start dynamisation Account component with state user

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,14 +1,35 @@
 // == Import : npm
+import { useSelector } from 'react-redux';
 // == Import : local
 import './account.scss';
 
 // == Composant
 
 function Account() {
+  const image = useSelector((state) => state.user.avatar.imageUrl);
+  const name = useSelector((state) => state.user.avatar.name);
+  const email = useSelector((state) => state.user.email);
+  const nom = useSelector((state) => state.user.lastName);
+  const prenom = useSelector((state) => state.user.firstName);
   return (
-    <div className="account">
-      <div className="account__information">
-        <h3 className="account__information--title">Mes informations</h3>
+    <div className="my-account">
+      <h3 className="my-account__title">Mes informations</h3>
+      <div className="my-account__information">
+        <div className="my-account__information--avatar">
+          <img className="my-account_information--avatar--image" src={image} alt={name} />
+        </div>
+        <p className="my-account__information--nom">{nom}</p>
+        <p className="my-account__information--prenom">{prenom}</p>
+        <p className="my-account__information--email">{email}</p>
+      </div>
+      <div className="my-account__avatar">
+        <p>Choix de l'avatar</p>
+      </div>
+      <div className="my-account__edit">
+        <p>Editer les informations</p>
+      </div>
+      <div className="my-account__password">
+        <p>editer le mot de passe</p>
       </div>
     </div>
   );

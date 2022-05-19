ff097913cd8fe9b2bba83d34f1c7c5747f79c228
francois-michaut <f.michaut@orange.fr>
2022-05-19 16:10:29 +0200

    Dices dynamic and sum of dices and caracteristics are ok

--- src/components/Creator/CaracteristicsPrincipals/index.js
+++ src/components/Creator/CaracteristicsPrincipals/index.js
@@ -4,103 +4,102 @@ import {
   saveConstitutionDice, saveDexterityDice, saveIntelligenceDice, saveStrengthDice, saveWisdomDice,
 } from '../../../actions/character';
 import throwDice from '../../../selectors/dice';
+import sum from '../../../selectors/sum';
 // import DiceButton from '../DiceButton';
 import './caracteristics.scss';
 
 function CaracteristicsPrincipals() {
   // Récupération des caractéristiques de la sous-race depuis le state
   const caracteristics = useSelector((state) => state.character.currentSubrace);
-
   const dexterityDiceResult = useSelector((state) => state.character.dexterityDice);
   const strengthDiceResult = useSelector((state) => state.character.strenghtDice);
   const constitutionDiceResult = useSelector((state) => state.character.constitutionDice);
   const intelligenceDiceResult = useSelector((state) => state.character.intelligenceDice);
   const wisdomDiceResult = useSelector((state) => state.character.wisdomDice);
   const charismaDiceResult = useSelector((state) => state.character.charismaDice);
-  console.log(dexterityDiceResult);
   const dispatch = useDispatch();
 
-  function dexterityDice() {
+  function dexterityDiceHandler() {
     const diceResult = throwDice();
     dispatch(saveDexterityDice(diceResult));
   }
-  function strengthDice() {
+  function strengthDiceHandler() {
     const diceResult = throwDice();
     dispatch(saveStrengthDice(diceResult));
   }
 
-  function constitutionhDice() {
+  function constitutionhDiceHandler() {
     const diceResult = throwDice();
     dispatch(saveConstitutionDice(diceResult));
   }
 
-  function intelligenceDice() {
+  function intelligenceDiceHandler() {
     const diceResult = throwDice();
     dispatch(saveIntelligenceDice(diceResult));
   }
 
-  function wisdomDice() {
+  function wisdomDiceHandler() {
     const diceResult = throwDice();
     dispatch(saveWisdomDice(diceResult));
   }
 
-  function charismaDice() {
+  function charismaDiceHandler() {
     const diceResult = throwDice();
     dispatch(saveCharismaDice(diceResult));
   }
-  return (
 
+  return (
     <section className="caracteristics__principals">
       <h4 className="caracteristics__principals--title">Caractéristiques principales avec bonus selon le choix de la classe:</h4>
       <ul className="caracteristics__principals--list">
-        <li className="caracteristics__principals--list--item">-Force: {caracteristics.strength} + {strengthDiceResult}
+        <li className="caracteristics__principals--list--item">-Force: {caracteristics.strength || '0'} + {strengthDiceResult || '0'} = {sum(caracteristics.strength, strengthDiceResult)}
           <button
             type="button"
-            onClick={strengthDice}
+            onClick={strengthDiceHandler}
           >
             lancer de dé
           </button>
         </li>
-        <li className="caracteristics__principals--list--item">-Dextérité: {caracteristics.dexterity} + {dexterityDiceResult}
+        <li className="caracteristics__principals--list--item">-Dextérité: {caracteristics.dexterity || '0' } + {dexterityDiceResult || '0'} = {sum(caracteristics.dexterity, dexterityDiceResult)}
           <button
             type="button"
-            onClick={dexterityDice}
-          >
-            lancer de dé
-          </button>
-
-        </li>
-        <li className="caracteristics__principals--list--item">-Constitution: {caracteristics.constitution} + {constitutionDiceResult}
-          <button
-            type="button"
-            onClick={constitutionhDice}
+            onClick={dexterityDiceHandler}
           >
             lancer de dé
           </button>
 
         </li>
-        <li className="caracteristics__principals--list--item">-Intelligence: {caracteristics.intelligence} + {intelligenceDiceResult}
+        <li className="caracteristics__principals--list--item">-Constitution: {caracteristics.constitution || '0'} + {constitutionDiceResult || '0'} = {sum(caracteristics.constitution, constitutionDiceResult)}
           <button
             type="button"
-            onClick={intelligenceDice}
+            onClick={constitutionhDiceHandler}
           >
             lancer de dé
           </button>
 
         </li>
-        <li className="caracteristics__principals--list--item">-Volonté: {caracteristics.wisdom} + {wisdomDiceResult}
+        <li className="caracteristics__principals--list--item">-Intelligence: {caracteristics.intelligence || '0'} + {intelligenceDiceResult || '0'} = {sum(caracteristics.intelligence, intelligenceDiceResult)}
           <button
             type="button"
-            onClick={wisdomDice}
+            onClick={intelligenceDiceHandler}
           >
             lancer de dé
           </button>
 
         </li>
-        <li className="caracteristics__principals--list--item">-Charisme: {caracteristics.charisma} + {charismaDiceResult}
+        <li className="caracteristics__principals--list--item">-Volonté: {caracteristics.wisdom || '0'} + {wisdomDiceResult || '0'} = {sum(caracteristics.wisdom, wisdomDiceResult)}
           <button
             type="button"
-            onClick={charismaDice}
+            onClick={wisdomDiceHandler}
+          >
+            lancer de dé
+          </button>
+
+        </li>
+        <li className="caracteristics__principals--list--item">-Charisme: {caracteristics.charisma || '0'} + {charismaDiceResult || '0'} = {sum(caracteristics.charisma, charismaDiceResult)}
+          <button
+            type="button"
+            onClick={charismaDiceHandler}
           >
             lancer de dé
           </button>
--- src/selectors/dice.js
+++ src/selectors/dice.js
@@ -3,8 +3,17 @@ export default function throwDice() {
   const secondDiceResult = Math.floor(Math.random() * (7 - 1)) + 1;
   const thirdDiceResult = Math.floor(Math.random() * (7 - 1)) + 1;
   const fourthDiceResult = Math.floor(Math.random() * (7 - 1)) + 1;
-  const threeThrows = firstDiceResult + secondDiceResult + thirdDiceResult;
+  // const threeThrows = firstDiceResult + secondDiceResult + thirdDiceResult;
   const dicesResultsList = [firstDiceResult, secondDiceResult, thirdDiceResult, fourthDiceResult];
+  const lowerDiceFromResults = Math.min(...dicesResultsList);
   console.log(dicesResultsList);
-  return threeThrows;
+  console.log(lowerDiceFromResults);
+  const lowerDice = (dice) => dice === lowerDiceFromResults;
+  const lowerDiceIndex = dicesResultsList.findIndex(lowerDice);
+  console.log(lowerDiceIndex);
+  dicesResultsList.splice(lowerDiceIndex, 1);
+  console.log(dicesResultsList);
+  const sumDices = dicesResultsList[0] + dicesResultsList[1] + dicesResultsList[2];
+  console.log(sumDices);
+  return sumDices;
 }

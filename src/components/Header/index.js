534973556fdc8c85b34672938792929276f2b04d
GacherBenoit <benoitgacher@gmail.com>
2022-05-20 15:36:23 +0200

    card text limit is ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,9 +7,6 @@ import './header.scss';
 function Header() {
   return (
     <header className="header">
-      <div className="header__content">
-        <h1 className="header__title">O'Dungeon</h1>
-      </div>
       <div className="header__navbar--left">
         <Navbar />
       </div>

d241fe68b2a86564c71a731d49db2362b00984a3
francois-michaut <f.michaut@orange.fr>
2022-05-22 12:29:25 +0200

    fix css on subclasse component on mobile and desktop

--- src/components/Subclasses/ClasseItem/index.js
+++ src/components/Subclasses/ClasseItem/index.js
@@ -7,29 +7,29 @@ function ClasseItem() {
   return (
     <div className="items">
       <h2 className="items__title">Spécificités de la classe</h2>
-      <p className="items__dices">Dé de vie: {currentClasse.lifeDice}</p>
+      <p className="items__dices">Dé de vie: <span className="items__result--span">{currentClasse.lifeDice}</span></p>
       <div className="items__armor">
         <h3 className="items__armor--title">Armure</h3>
         {(currentClasse.armors).map((armor) => (
           <div key={armor.id}>
-            <p className="items__spec">Nom de l'armure: {armor.name}</p>
-            <p className="items__spec">Type d'armure : {armor.armorType}</p>
-            <p className="items__spec">Classe d'armure : {armor.armorClass}</p>
-            <p className="items__spec">Discrétion : {armor.discretion ? armor.discretion : 'aucune'}</p>
-            <p className="items__spec">Poids : {armor.weight}kg </p>
-            <p className="items__spec">Force : {armor.strength}</p>
+            <p className="items__spec">Nom de l'armure:<span className="items__result--span"> {armor.name}</span></p>
+            <p className="items__spec">Type d'armure : <span className="items__result--span">{armor.armorType}</span></p>
+            <p className="items__spec">Classe d'armure : <span className="items__result--span">{armor.armorClass}</span></p>
+            <p className="items__spec">Discrétion : <span className="items__result--span">{armor.discretion ? armor.discretion : 'aucune'}</span></p>
+            <p className="items__spec">Poids : <span className="items__result--span">{armor.weight}kg</span> </p>
+            <p className="items__spec">Force : <span className="items__result--span">{armor.strength}</span></p>
           </div>
         ))}
         <h3 className="items__weapon--title">Arme</h3>
         <div className="items__weapons">
           {(currentClasse.weapons).map((weapon) => (
             <div key={weapon.id}>
-              <p className="items__spec">Nom de l'arme : {weapon.name}</p>
-              <p className="items__spec">Type de l'arme : {weapon.type} </p>
-              <p className="items__spec">Propriété de l'arme : {weapon.property} </p>
-              <p className="items__spec">Dé de dommages : {weapon.damageDice} </p>
-              <p className="items__spec">Type de dommages : {weapon.damageType} </p>
-              <p className="items__spec">Poids de l'arme : {weapon.weight} </p>
+              <p className="items__spec">- Nom de l'arme : <span className="items__result--span">{weapon.name}</span></p>
+              <p className="items__spec">Type de l'arme : <span className="items__result--span">{weapon.type}</span> </p>
+              <p className="items__spec">Propriété de l'arme : <span className="items__result--span">{weapon.property}</span> </p>
+              <p className="items__spec">Dé de dommages : <span className="items__result--span">{weapon.damageDice}</span> </p>
+              <p className="items__spec">Type de dommages :<span className="items__result--span"> {weapon.damageType}</span> </p>
+              <p className="items__spec">Poids de l'arme :<span className="items__result--span"> {weapon.weight}</span> </p>
             </div>
           ))}
         </div>
@@ -37,9 +37,9 @@ function ClasseItem() {
         <div className="items__class-items">
           {(currentClasse.playableClassItems).map((item) => (
             <div key={item.item.id}>
-              <p className="items__spec">Nom de l'objet :{item.item.name}</p>
-              <p className="items__spec">Quantité possédée : {item.quantity} </p>
-              <p className="items__spec">Poids de l'objet :{item.item.weight}Kg</p>
+              <p className="items__spec">- Nom de l'objet :<span className="items__result--span">{item.item.name}</span></p>
+              <p className="items__spec">Quantité possédée :<span className="items__result--span"> {item.quantity}</span> </p>
+              <p className="items__spec">Poids de l'objet :<span className="items__result--span">{item.item.weight}Kg</span></p>
             </div>
           ))}
         </div>

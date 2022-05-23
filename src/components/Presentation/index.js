555676fc2eb59fb17894b3d090d85429caf1d1e4
AlbanBerthn <a.bertholon69@gmail.com>
2022-05-23 10:05:42 +0200

    change text of componement presentation and Title for classe card and race card

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -37,22 +37,12 @@ function Presentation() {
             Présentation
           </h2>
           <p className="main__section--presentation--paragraphe">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed neque nulla, volutpat at dui quis, consectetur mattis arcu.
-            Vivamus quis lectus dui.
-            Al Nunc auctor neque sed est pellentesque, a ultricies elit porttitor.
-            Pellentesque semper pharetra urna id finibus.
-            Nullam sed mollis sem.
-            Ut leo est, viverra a quam quis, accumsan placerat massa.
-            Morbi vitae elit nisl.
-            In nec erat eget tortor accumsan imperdiet non commodo orci.
-            Quisque sem dolor, euismod sit amet ultrices a, bibendum a odio.
-            Nam lacinia interdum lorem, id suscipit purus mollis eu.
-            Mauris non tincidunt nisl, ut interdum risus. Mauris nibh elit, pharetra
-            quis vulputate id, volutpat eu libero. Pellentesque id porttitor diam. Aliquam
-            aliquet dignissim ante. Donec auctor sem dapibus sapien finibus, eget cursus neque
-            vestibulum. Sed mollis efficitur dui, at suscipit lectus. Mauris mollis aliquam
-            interdum. Maecenas eu nunc tempus, dictum lorem sit amet, viverra enim. Mauris laoreet.
+            Donjon et Dragon est un jeu de rôle sur table.
+            C'est un sous-genre du jeu de société qui se base sur le principe de narration commune :
+            vouz créez une histoire à plusieurs au fil de vos actions.
+            La plupart du temps les joueurs coopèrent tous ensemble
+            afin d'aller jusqu'au bout d'une aventure,
+            guidés par le Maître du Jeu (MJ).
           </p>
           <div className="main__section--presentation--link--contener">
             <NavLink className="main__section--presentation--link" to="/regles-et-univers"> En savoir plus....</NavLink>
@@ -63,18 +53,11 @@ function Presentation() {
             Règles du jeu
           </h2>
           <p className="main__rules--paragraphe">
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Rerum praesentium sint quam,
-            reiciendis atque reprehenderit unde cumque impedit! Nam quas at veritatis quae sit
-            odit illo architecto quaerat alias modi.
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Rerum praesentium sint quam,
-            reiciendis atque reprehenderit unde cumque impedit! Nam quas at veritatis quae sit
-            odit illo architecto quaerat alias modi.
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Rerum praesentium sint quam,
-            reiciendis atque reprehenderit unde cumque impedit! Nam quas at veritatis quae sit
-            odit illo architecto quaerat alias modi.
+            Le jeu de rôle Dungeons & Dragons baigne dans un univers de combats et de magie.
+            De nombreux jeux de notre enfance sont basés sur l’immersion.
+            Au même titre, D&D s’appuie sur l’imagination.
+            Il s'agit de détailler le château fort par une nuit de tempête
+            et imaginer comment un aventurier réagirait aux défis que représente cette scène.
           </p>
           <div className="main__rules--contener">
             <NavLink className="main__section--presentation--link" to="/regles-et-univers"> En savoir plus....</NavLink>
@@ -83,7 +66,7 @@ function Presentation() {
       </div>
       <div className="main__random">
         <h3 className="main__random--title">
-          Classes au hasard
+          Découvrir une Classe
         </h3>
         <div className="main__random--classes">
           { randomClasses.map((classeRandom) => (
@@ -99,7 +82,7 @@ function Presentation() {
           ))}
         </div>
         <h3 className="main__random--title">
-          Races au hasard
+          Découvrir une Race
         </h3>
         <div className="main__random--races">
           { randomRace.map((raceRandom) => (
